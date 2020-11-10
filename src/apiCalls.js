7fedd9570115a8483410133f49779d9dfae0e89b
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-11-10 11:38:57 -0700

    Build new POST function in apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,8 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (longUrl, title) => {
+  return fetch('http://localhost:3001/api/vi/urls', { method: 'POST', body: {long_url: longUrl, title: title} })
+    .then(response => response.json())
+}
